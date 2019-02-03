d8d0b8e8125c4cf3fad3ad378aa838ba63c9bcc1
TrickyCmd <info@viraledge.co.uk>
2019-02-03 15:35:48 +0000

    feat: adding css modules

--- app/src/Components/Button/Button.js
+++ app/src/Components/Button/Button.js
@@ -1,10 +1,12 @@
+import styles from './Button.module.css'
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 
+console.log(styles)
 const Button = ({ id, type = 'button', onClick, label = 'no label set...' }) => {
   return (
-    <div className={`rounded-button__container`}>
-      <button id={id} className={'rounded-button'} type={type} onClick={onClick}>{label}</button>
+    <div className={styles['button-container']}>
+      <button id={id} className={styles.button} type={type} onClick={onClick}>{label}</button>
     </div>
   )
 }
@@ -15,5 +17,6 @@ Button.propTypes = {
   onClick: PropTypes.func
 }
 
+// names and memo'd export to allow proper unit testing with enzyme https://github.com/airbnb/enzyme/issues/1875
 export { Button }
 export default memo(Button)
