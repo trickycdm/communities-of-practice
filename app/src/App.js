3b3e6ee138cc3598473c2cb8d16a23d3df5c7882
Kevin Wilson <kevin@partcoffee.com>
2020-04-05 21:07:01 +0100

    refactor: Update to lazy route routes
    Overkill for something this small but just as a POC to make sure it works within the context of this app.

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, lazy, Suspense } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Topbar from 'Components/Topbar/Topbar'
 import PrivateRoute from 'Components/PrivateRoute'
-import Login from 'Views/Login/Login'
 import './style.scss'
 
 const Home = () => <h2>Homepage</h2>
 const PrivatePage = () => <h2>Private</h2>
+const Login = lazy(() => import('./Views/Login/Login'))
+
+const Loader = () => <>Loading&hellip;</>
 
 class App extends Component {
   render () {
@@ -14,9 +16,12 @@ class App extends Component {
       <Router>
         <div className='main-container'>
           <Topbar />
-          <Route path='/' exact component={Home} />
-          <Route path='/login/' component={Login} />
-          <PrivateRoute path='/private' authenticated={false} component={PrivatePage} />
+
+          <Suspense fallback={<Loader />}>
+            <Route path='/' exact component={Home} />
+            <Route path='/login/' component={Login} />
+            <PrivateRoute path='/private' authenticated={false} component={PrivatePage} />
+          </Suspense>
         </div>
       </Router>
     )
