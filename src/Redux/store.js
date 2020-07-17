f766d73eb4895c7f76f84b2481a96b84794515d0
TrickyCmd <info@viraledge.co.uk>
2020-07-17 09:10:36 +0100

    fix: formatting

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,20 +1,20 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import user from './user';
-import communities from './communities';
-import meta from './meta';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import user from './user'
+import communities from './communities'
+import meta from './meta'
 
 const reducers = combineReducers({
   user,
   communities,
   meta,
-});
+})
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default createStore(
   reducers,
   composeEnhancers(
     applyMiddleware(thunk),
   ),
-);
+)
