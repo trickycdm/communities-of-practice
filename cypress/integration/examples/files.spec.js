d57d40c69754fd1fc7d21e5301b67b519b688d3f
Kevin Wilson <kevin@partcoffee.com>
2020-08-16 20:08:13 +0100

    chore: Fix linting across Cypress

--- cypress/integration/examples/files.spec.js
+++ cypress/integration/examples/files.spec.js
@@ -96,6 +96,7 @@ context('Files', () => {
         cy.writeFile('cypress/fixtures/users.json', response.body)
       })
     cy.fixture('users').should((users) => {
+      // eslint-disable-next-line no-unused-expressions
       expect(users[0].name).to.exist
     })
 
